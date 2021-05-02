9398a97106996ee3fe6de766ebf492b9fde52ff6
Omer Acar <omeracar0@gmail.com>
2021-05-01 22:30:35 -0400

    org mode to docx scripts

--- org_to_word.sh
+++ org_to_word.sh
@@ -4,15 +4,14 @@ pandoc "$infile" \
     -s  \
     --reference-doc=/Users/omeracar/Sync/Docs/github/pandoc/template.docx \
     -F /Users/omeracar/.local/bin/pandoc-crossref  \
-    -M suppress-bibliography=true \
     --citeproc \
     --bibliography=/Users/omeracar/Nextcloud/bibliography.bib \
-    -f markdown \
+    -f org \
     -t docx \
+    -M figPrefix="Figure" \
     --csl=/Users/omeracar/Zotero/styles/nature.csl \
     --pdf-engine=xelatex --mathjax -o "$outfile"
-
-
+    #-M suppress-bibliography=true \
 #    --filter /Users/omeracar/Downloads/pandoc-2.10/bin/pandoc-citeproc \
     #--lua-filter=/Users/omeracar/Docs/zotero.lua \
 
--- zotero_live_word.sh
+++ zotero_live_word.sh
@@ -3,8 +3,8 @@ export infile=$1
 export outfile=$2
 pandoc "$infile" \
     -s  \
-    --lua-filter=/Users/omeracar/Docs/zotero.lua \
-    --reference-doc=/Users/omeracar/Docs/github/pandoc/template.docx \
+    --lua-filter=/Users/omeracar/Sync/Docs/zotero.lua \
+    --reference-doc=/Users/omeracar/Sync/Docs/github/pandoc/template.docx \
     -F /Users/omeracar/.local/bin/pandoc-crossref  \
     -M suppress-bibliography=true \
     -f markdown \
